f179fde5f3a3b127ec68963a4f90f9a6d33d543c
PascalGoergen <pascal@ungeordnet.de>
2014-05-14 21:53:09 +0200

    Overview Layout
    Starting the Layout for the Overview. Navbar with transition

--- index.php
+++ index.php
@@ -1,65 +1,70 @@
 <?php 
- error_reporting(E_ALL); 
- ini_set("display_errors", 1); 
+error_reporting(E_ALL); 
+ini_set("display_errors", 1); 
 include ('classes/controller.php');
 $controller = new controller();
 ?>
 <!DOCTYPE html> 
 <html lang="de">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>CIS Check</title>
+    <title>Notify</title>
 
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/dashboard.css" rel="stylesheet">
-	<link href="css/landing-page.css" rel="stylesheet">
+    <link rel="stylesheet" href = "/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href = "/css/main.css" />
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-	<style>
-	.filter-table .quick { margin-left: 0.5em; font-size: 0.8em; text-decoration: none; }
-	.fitler-table .quick:hover { text-decoration: underline; }
-	td.alt { background-color: #ffc; background-color: rgba(255, 255, 0, 0.2); }
-	</style> 
-	<script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+      <![endif]-->
+      <style>
+      .filter-table .quick { margin-left: 0.5em; font-size: 0.8em; text-decoration: none; }
+      .fitler-table .quick:hover { text-decoration: underline; }
+      td.alt { background-color: #ffc; background-color: rgba(255, 255, 0, 0.2); }
+      </style> 
+      <script src="js/jquery.min.js"></script>
+      <script src="js/bootstrap.min.js"></script>
   </head>
 
   <body>
-  
-  <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                    <span class="sr-only">CIS Check</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand">CIS Check</a>
-            </div>
 
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse navbar-right navbar-ex1-collapse">
-                <ul class="nav navbar-nav">
-                    <li><a href="?">Anmeldung</a>
-                    </li>
-                </ul>
+      <nav>
+        <div class='navbar navbar-inverse navbar-fixed-top' style='background-color: #036; -moz-transition: height 0.5s linear;' role='navigation'>
+            <div id="nav" class='container'>
+                <div class='navbar-collapse collapse'>
+                    <div class="btn btn-primary btn-lg" style="background-color: #036; border-color: #036;">
+                        <span class="glyphicon glyphicon-th-list"></span>
+                    </div>              
+                </div>
+                <div id="navhiddenbar">
+                    <form class="form-inline" role="form">
+                        <div class="form-group">
+                            <label class="sr-only" for="exampleInputEmail2">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Enter email">
+                        </div>
+                        <div class="form-group">
+                            <label class="sr-only" for="exampleInputPassword2">Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password">
+                        </div>
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox"> Remember me
+                            </label>
+                        </div>
+                        <button type="submit" class="btn btn-default">Sign in</button>
+                    </form>
+
+                </div>
             </div>
-            <!-- /.navbar-collapse -->
         </div>
-        <!-- /.container -->
     </nav>
+    <?php include ($controller->get_site($controller)); ?>
 
-	<?php include ($controller->get_site($controller)); ?>
-
-  </body>
+</body>
 </html>
--- views/overview.php
+++ views/overview.php
@@ -7,38 +7,51 @@
 
         <div class="container">
 <?php if(!empty($return)){include("error/".$return.".htm");}?>
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="intro-message">
-                        <h1>[Name]</h1>
-                        <h3>[Slogan]</h3>
-                        <hr class="intro-divider">
-                        <ul class="list-inline intro-social-buttons">
-						<form method="POST" action="?site=overview&action=register">
-                            <label for="cis_nummer">CIS-Nummer</label><input class="form-control landing-input" placeholder="CIS-Nummer" name="cis_number"><br>
-							<label for="cis_nummer">CIS-Passwort</label><input type="password" class="form-control landing-input" placeholder="CIS-Passwort" name="cis_password">
-							<label for="cis_nummer">[Name]-Passwort</label><input type="password" class="form-control landing-input" placeholder="CIS-Passwort" name="password">
-						</ul>
-						<input type="submit" class="btn btn-default btn-lg" value="Eintragen"></input>
-						</form>
-						
-                    </div>
-                </div>
+<head>
+    <meta charset="utf-8">
+
+    <title>Notify</title>
+    <meta name="description" content="Notify">
+    <meta name="author" content="SitePoint">
+
+    <link rel="stylesheet" href = "/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href = "/css/main.css" />
+
+</head>
+
+<body role="document">
+    <div class='container theme-showcase' role='main'>
+        <div class="jumbotron">
+            <div class="intro-message">
+                <h1>NOTIFY</h1>
+                <h3>NOTIFY. Clever gemacht.</h3>
+                <hr class="intro-divider">
+                <ul class="list-inline intro-social-buttons">
+                    <form method="POST" action="?site=overview&action=register">
+                        <label for="cis_nummer">CIS-Nummer</label><input class="form-control landing-input" placeholder="CIS-Nummer" name="cis_number"><br>
+                        <label for="cis_nummer">CIS-Passwort</label><input type="password" class="form-control landing-input" placeholder="CIS-Passwort" name="cis_password">
+                        <label for="cis_nummer">NOTIFY-Passwort</label><input type="password" class="form-control landing-input" placeholder="Passwort" name="password">
+                    </ul>
+                    <input type="submit" class="btn btn-default btn-lg" value="Eintragen"></input>
+                </form>
+
             </div>
-
         </div>
-
     </div>
 
-    <div class="content-section-a">
+</div>
 
-        <div class="container">
+</div>
 
-            <div class="row">
-                <div class="col-lg-5 col-sm-6">
-                    <hr class="section-heading-spacer">
-                    <div class="clearfix"></div>
-                    <h2 class="section-heading">Infos über deine Noten:
+<div class="content-section-a">
+
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-5 col-sm-6">
+                <hr class="section-heading-spacer">
+                <div class="clearfix"></div>
+                <h2 class="section-heading">Infos über deine Noten:
                     <p class="lead">Wir prüfen nach deiner Anmeldung das CIS jede Minute auf eine neue Note. Sollte es eine geben, bekommst sofort eine Mail mit deiner Note und deinem aktuellen Schnitt mit Creditpointgewichtung. </p>
                 </div>
                 <div class="col-lg-5 col-lg-offset-2 col-sm-6">
@@ -61,9 +74,9 @@
                 </div>
                 <div class="col-lg-6">
                     <ul class="list-inline banner-social-buttons">
-					    <li><a href="https://github.com/peterjl/cis_check" class="btn btn-default btn-lg">Backend</a>
+                        <li><a href="https://github.com/peterjl/cis_check" class="btn btn-default btn-lg">Backend</a>
                         </li>
-						<li><a href="https://github.com/PascalGoergen/CISServiceFrontend" class="btn btn-default btn-lg">Frontend</a>
+                        <li><a href="https://github.com/PascalGoergen/CISServiceFrontend" class="btn btn-default btn-lg">Frontend</a>
                         </li>
                     </ul>
                 </div>
@@ -74,16 +87,16 @@
 
     </div>
     <!-- /.banner -->
-
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <ul class="list-inline">
-                        <li><a href="?">Home</a>
-                        </li>
-                    </ul>
-                </div>
+</body>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <ul class="list-inline">
+                    <li><a href="?">Home</a>
+                    </li>
+                </ul>
             </div>
         </div>
-    </footer>
+    </div>
+</footer>   
