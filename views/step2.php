5181a61fd55fde76817845e5a7fb4aba29aca7d7
Jan <jackflash5500@gmail.com>
2014-05-15 21:17:18 +0200

    Added Step 2 text

--- views/step2.php
+++ views/step2.php
@@ -16,16 +16,22 @@
 			<div class="col-lg-6 col-sm-6">
 				<div class="clearfix"></div>
 				<!--<img src="img/notify.png"/>-->
-				<h2 class="section-heading">Infos Ã¼ber diese Seite:</h2>
-				<p>Hier kÃ¶nnen die ErklÃ¤rungen Ã¼ber die einzutragenden Felder stehen, sowie die EinverstÃ¤ndniserklÃ¤rung.</p>
+				<h2 class="section-heading">Infos über diesen Service:</h2>
+				<h3>3 wichtige Fakten über Notify</h3>
+				<p>1.Notify ist völlig kostenlos und wird es auch bleiben</p>
+				<p>2.Notify speichert dein CIS-Passwort in einer internen Datenbank. Dein Passwort ist gesalted und gehasht und damit nicht mehr lesbar, selbst für uns nicht.</p>
+				<p>3.Damit Notify immer sofort reagieren kann, loggt sich Notify alle paar Minuten mit deinem Account im CIS ein und schaut, ob es etwas neues gibt.</p>
+				<br>
 			</div>
 			<div class="jumbotron col-sm-6">
 				<div class="intro-message">
-					<h2>NOTIFY Registrierung Schritt 2:</h2>
+					<h2>NOTIFY Registrierung Schritt 2:<small>(Fast geschafft!)</small></h2>
 					<hr class="intro-divider">
 					<form method="POST" action="?site=settings&action=register">
-						<h2>Hallo, <?php echo $name[0]?></h2>
-						<label for="email">E-Mail</label><input type="email" class="form-control landing-input" placeholder="anne.theke@nordakademie.de" name="email"><br>
+						<h3>Hallo <?php echo $name[0]?>,</h3>
+						<label for="email">E-Mail für die Benachrichtigungen</label><input type="email" class="form-control landing-input" placeholder="anne.theke@nordakademie.de" name="email"><br>
+						<input type="checkbox" id="agb">Mit dem Bestätigen der dieser Checkbox bist du mit der Art und Weise wie Notify funktioniert einverstanden und akzeptierst das wir keine Haftung für deine Daten übernehmen.</input>
+						<br><br>
 						<div class="btn-group btn-group-justified">
 							<div class="btn-group">
 								<input type="submit" class="btn btn-success btn-lg" value="#Geilon"></input>
