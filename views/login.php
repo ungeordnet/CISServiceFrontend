96180c6f27026119b98f1be5c120344b4fa8d67c
PascalGoergen <pascal@ungeordnet.de>
2014-05-15 17:25:37 +0200

    Edit
    Edit the Login-Site (login.php). New Design. ToDo: Edit the text on the
    right site

--- views/login.php
+++ views/login.php
@@ -3,28 +3,52 @@
 		$return = $controller->doLogin($_GET['id'], $_POST['passwort']);
 	}
 ?>
-<div class="container">
-	<div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">                    
-		<?php if(!empty($return)){include("error/".$return.".htm");}?>
-		<div class="panel panel-info" >
-			<div class="panel-heading">
-				<div class="panel-title">Login</div>
-			</div>     
-			<div style="padding-top:30px" class="panel-body" >
-				<div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
-				<form id="loginform" action="?site=login&id=<?php echo $_GET['id'];?>" method="post" class="form-horizontal" role="form">
-					<div style="margin-bottom: 25px" class="input-group">
-						<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-						<input id="login-password" type="password" class="form-control" name="passwort" placeholder="Passwort">
-					</div>
-					<div style="margin-top:10px" class="form-group">
-						<!-- Button -->
-						<div class="col-sm-12 controls">
-						  <input id="btn-login" type="submit" class="btn btn-success" value="Login"></input>
-						</div>
-					</div>
-				</form>
+<body role="document">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6 col-sm-6">
+				<div class="clearfix"></div>
+				<!--<img src="img/notify.png"/>-->
+				<h2 class="section-heading">Infos über diese Seite:</h2>
+				<p>Hier können die Erklärungen über die einzutragenden Felder stehen, sowie die Einverständniserklärung.</p>
 			</div>
-		</div>  
+			<div class="jumbotron col-sm-6">
+				<div class="intro-message">
+					<h2>NOTIFY Registrierung Schritt 2:</h2>
+					<hr class="intro-divider">
+					<form method="POST" action="?site=overview&action=register">
+						<label for="name">Name</label><input type="text" class="form-control landing-input" placeholder="Anne" name="name"><br>
+						<label for="surname">Nachname</label><input type="text" class="form-control landing-input" placeholder="Theke" name="surname"><br>
+						<label for="email">E-Mail</label><input type="email" class="form-control landing-input" placeholder="anne.theke@nordakademie.de" name="email"><br>
+						<div class="btn-group btn-group-justified">
+							<div class="btn-group">
+								<input type="submit" class="btn btn-success btn-lg" value="#Geilon"></input>
+							</div>
+							<div class="btn-group">
+								<input type="submit" class="btn btn-danger btn-lg" value="Nee, lieber nicht"></input>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
 	</div>
-</div>
+
+	<br>
+</body>
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<ul class="list-inline">
+					<li><a href="?">Home</a>
+						<li>Verbessere den Code auf Github:
+							<a href="https://github.com/peterjl/cis_check" class="btn btn-default btn-sm">Backend</a>
+							<a href="https://github.com/PascalGoergen/CISServiceFrontend" class="btn btn-default btn-sm">Frontend</a>
+						</li>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</footer>   
